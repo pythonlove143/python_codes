b20167e70c1ed74c42dbaaf779203a708587eddd
Sarath Mohan <Sarath Mohan>
2017-08-16 15:36:57 -0700

    Add the python codes for June 20

---
+++ median.py
@@ -0,0 +1,54 @@
+# Define a procedure, median, that takes three
+# numbers as its inputs, and returns the median
+# of the three numbers.
+
+# Make sure your procedure has a return statement.
+
+def bigger(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+def biggest(a,b,c):
+    return bigger(a,bigger(b,c))
+
+def median(a,b,c):
+    p = biggest(a,b,c)
+    if (p == a):
+        q = bigger(b,c)
+    if (p == b):
+        q = bigger(a,c)
+    if (p == c):
+        q = bigger(a,b)
+    return q
+        
+        
+    
+#    d = bigger(a,b)
+#    e = bigger(b,c)
+#    f = bigger(a,c)
+#    if(d>e):
+#        if(e>f):
+#            return f
+#        else:
+#            return e
+#    else:
+#        if(d>f):
+#            return f
+#        else:
+#            return d
+
+
+print(median(1,2,3))
+#>>> 2
+
+print(median(9,3,6))
+#>>> 6
+
+print(median(7,8,7))
+#>>> 7
+
+
+
+
